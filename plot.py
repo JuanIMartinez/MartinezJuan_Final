902a648d3775b90de5855dec74ea8ae1eae88442
JuanIMartinez <42219545+JuanIMartinez@users.noreply.github.com>
2019-05-23 11:52:30 -0500

    Add files via upload

--- plot.py
+++ plot.py
@@ -5,10 +5,6 @@ from mpl_toolkits.mplot3d import axes3d
 x=np.genfromtxt("datos.dat",usecols=(0))
 y=np.genfromtxt("datos.dat",usecols=(1))
 
-
-
-
-
 plt.figure(figsize=[10,10])
 plt.plot(x,y, c="royalblue", label="trayectoria", linewidth=0.7)
 plt.scatter(x,y, c="r", label="steps", s=5.)
@@ -16,6 +12,7 @@ plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Desplazamientoen el tiempo")
 plt.legend()
+plt.xlim(0,2)
 plt.grid()
 plt.savefig("MartinezJuan_final_15.pdf")
 plt.close()
